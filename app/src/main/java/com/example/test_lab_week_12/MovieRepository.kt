75c1452a94ca9ae6201cfa9e5091972e8197c588
DillonDONE <dillon.donetta@student.umn.ac.id>
2025-12-07 21:00:53 +0700

    add file

---
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -0,0 +1,19 @@
+package com.example.test_lab_week_12
+
+import com.example.test_lab_week_12.api.MovieService
+import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class MovieRepository(private val movieService: MovieService) {
+
+    private val apiKey = "7a62f0245b86d5af7f144e0137459a6a"
+
+    fun fetchMovies(): Flow<List<Movie>> {
+        return flow {
+            emit(movieService.getPopularMovies(apiKey).results)
+        }.flowOn(Dispatchers.IO)
+    }
+}
